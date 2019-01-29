35d630e554729a553becaeea34275e5517a19bbf
KrapivinIhor <ihorkrapivin@gmail.com>
2019-01-29 16:40:09 +0200

    HW2

---
+++ src/com/company/ImmutableClass/Ferrari.java
@@ -0,0 +1,31 @@
+package com.company.ImmutableClass;
+
+public class Ferrari {
+    private final int speed;
+    private  final String color;
+    private final int horsePower;
+    private final double pPM;
+
+    public Ferrari(final int speed, final String color, final int horsePower, final double pPM) {
+        this.speed = speed;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.pPM = pPM;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public double getpPM() {
+        return pPM;
+    }
+}
