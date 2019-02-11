9e65dc226dc3acf1c6509bcaca4ace3c4238c68a
ihor <ihorkrapivin@gmail.com>
2019-02-11 19:38:07 +0200

    add all changes

--- src/com/company/PatternBuilder/Main.java
+++ src/com/company/PatternBuilder/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        final Person person = new PersonBuilder()
+        final Person person = new Person.PersonBuilder()
                                     .name("Jon")
                                     .age(18)
                                     .height(178.5)
